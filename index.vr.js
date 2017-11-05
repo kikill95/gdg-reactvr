25a71fd76264f0a7f736a36021e45d96807cb5db
Light <kirill.gus@gmail.com>
2017-11-05 16:42:39 +0200

    Add enemies as red spheres with random coordinates and with timeout

--- index.vr.js
+++ index.vr.js
@@ -4,6 +4,7 @@ import {
   asset,
   Pano,
   View,
+  Sphere,
   Sound
 } from 'react-vr'
 
@@ -12,16 +13,54 @@ export default class gdgReactVr extends React.Component {
     super(props)
 
     this.state = {
-      isAudioReady: false
+      isAudioReady: false,
+      enemies: []
     }
 
+    this.renderEnemies = this.renderEnemies.bind(this)
     this.startAudio = this.startAudio.bind(this)
+    this.addNewEnemy = this.addNewEnemy.bind(this)
+
+    setTimeout(this.addNewEnemy, 1000)
   }
 
   startAudio (event) {
     if (event.nativeEvent.playStatus === 'ready') {
       this.setState({isAudioReady: true})
     }
+  }
+
+  addNewEnemy () {
+    this.setState({
+      enemies: this.state.enemies.concat([{
+        x: Math.random() * 100 - 50,
+        y: Math.random() * 40 - 20,
+        z: (this.state.enemies.length % 2 ? 1 : -1) * (Math.random() * 13 + 7)
+      }])
+    }, () => {
+      setTimeout(this.addNewEnemy, 2000)
+    })
+  }
+
+  renderEnemies () {
+    return (this.state.enemies.map((enemy, index) => {
+      return (
+        <Sphere
+          key={index}
+          radius={1}
+          widthSegments={50}
+          heightSegments={50}
+          style={{
+            color: 'red',
+            transform: [{
+              translate: [enemy.x, enemy.y, enemy.z],
+              rotateX: enemy.x,
+              rotateY: enemy.y
+            }]
+          }}
+        />
+      )
+    }))
   }
 
   render () {
@@ -35,7 +74,8 @@ export default class gdgReactVr extends React.Component {
           playControl={this.state.isAudioReady ? 'play' : 'stop'}
           onPlayStatusChange={this.startAudio}
         />
-        <Pano source={asset('space2.jpg')} />
+        <Pano source={asset('space2.jpg')}/>
+        {this.renderEnemies()}
       </View>
     )
   }
